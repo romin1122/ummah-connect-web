b4985f6b30c1bb521f236b2c61930a00a440f04a
Romin <muhymenul2003@gmail.com>
2024-10-15 20:21:54 +0600

    Add environment variables instead of raw data

--- api/db.js
+++ api/db.js
@@ -1,8 +1,10 @@
 import mysql from 'mysql';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'lama',
-  password: 'romin1122',
-  database: 'lama',
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
 });
