450c8d487d1108d1b92908973ab6e246f0d4a3bc
Romin <muhymenul2003@gmail.com>
2024-10-19 21:29:35 +0600

    Added data validation in login and register

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -5,8 +5,45 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const validateName = (name) => {
+  console.log(name);
+  return name && name.trim().length > 0 && /^[a-zA-Z ]+$/.test(name);
+};
+
+const validateEmail = (email) => {
+  return (
+    email &&
+    email.trim().length > 0 &&
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    )
+  );
+};
+
+const validateUsername = (username) => {
+  return (
+    username &&
+    username.trim().length > 0 &&
+    /^(?=.{3,32}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/.test(username)
+  );
+};
+
+const validatePassword = (password) => {
+  return (
+    password && password.trim().length >= 8 && password.trim().length <= 32
+  );
+};
+
 export const register = (req, res) => {
   const q = 'SELECT * FROM users WHERE username = ?';
+
+  if (
+    !validateUsername(req.body.username) ||
+    !validatePassword(req.body.password) ||
+    !validateEmail(req.body.email) ||
+    !validateName(req.body.name)
+  )
+    return res.status(400).json('Invalid input');
 
   db.query(q, [req.body.username], (err, data) => {
     if (err) return res.status(500).json(err);
@@ -19,8 +56,8 @@ export const register = (req, res) => {
       'INSERT INTO users (`username`, `email`, `password`, `name`, `profilePic`) VALUE (?)';
 
     const values = [
-      req.body.username,
-      req.body.email,
+      req.body.username.toLowerCase(),
+      req.body.email.toLowerCase(),
       hashedPassword,
       req.body.name,
       '/static/defaultProfile.png',
@@ -34,8 +71,15 @@ export const register = (req, res) => {
 };
 
 export const login = (req, res) => {
+  console.log(validateUsername(req.body.username));
+  if (
+    !validateUsername(req.body.username) ||
+    !validatePassword(req.body.password)
+  )
+    return res.status(400).json('Invalid input');
+
   const q = 'SELECT * FROM users WHERE username = ?';
-  db.query(q, [req.body.username], (err, data) => {
+  db.query(q, [req.body.username.toLowerCase()], (err, data) => {
     if (err) return res.status(500).json(err);
     if (data.length === 0) return res.status(404).json('User not found!');
 
@@ -47,10 +91,12 @@ export const login = (req, res) => {
     if (!checkPassword)
       return res.status(400).json('Wrong username or password!');
 
-    const token = jwt.sign({ id: data[0].id }, process.env.ACCESS_TOKEN_KEY);
+    const token = jwt.sign({ id: data[0].id }, process.env.ACCESS_TOKEN_KEY, {
+      expiresIn: '30d',
+    });
 
     const { password, id, ...others } = data[0];
-    res
+    return res
       .cookie('accessToken', token, {
         httpOnly: true,
       })
